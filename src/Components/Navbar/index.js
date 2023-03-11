0226d8eb45f92ee9fc91936145df1f595d512083
ghosttyE <75579221+ghosttyE@users.noreply.github.com>
2023-03-10 22:27:27 -0300

    fix routes 2

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,9 +3,9 @@ import './style.css'
 export default function Navbar() {
     return(
         <div className="Navbar">
-            <a href='https://anthony-amla.github.io/rick-morty-api-consume/' className="Navbar-Option">Home</a>
-            <a href='https://anthony-amla.github.io/rick-morty-api-consume/characters' className="Navbar-Option">Characters</a>
-            <a href='https://anthony-amla.github.io/rick-morty-api-consume/episodes' className="Navbar-Option">Episodes</a>
+            <a href='/rick-morty-api-consume/' className="Navbar-Option">Home</a>
+            <a href='/rick-morty-api-consume/characters' className="Navbar-Option">Characters</a>
+            <a href='/rick-morty-api-consume/episodes' className="Navbar-Option">Episodes</a>
         </div>
     )
 }
